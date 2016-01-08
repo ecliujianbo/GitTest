fbfbcb9a669aefb281214cd06f0cb0d985f2470a
ecliujianbo <ecliujianbo@126.com>
2016-01-08 13:49:35 +0800

    add companyActivity

--- app/src/main/java/com/liu/gittest/MainActivity.java
+++ app/src/main/java/com/liu/gittest/MainActivity.java
@@ -1,14 +1,16 @@
 package com.liu.gittest;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,28 +18,11 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void showToast(View view){
-        Toast.makeText(this,"edit in home",Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void showToast(View view) {
+        Toast.makeText(this, "edit in home", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void toCompany(View view) {
+        startActivity(new Intent(this, CompanyActivity.class));
     }
 }
