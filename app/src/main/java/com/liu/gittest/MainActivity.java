9d587d98d77c534d646e7079e4a0810090dca720
zhangxin <zhangxin@youpu.cn>
2016-01-09 19:08:23 +0800

    去掉v7包

--- app/src/main/java/com/liu/gittest/MainActivity.java
+++ app/src/main/java/com/liu/gittest/MainActivity.java
@@ -1,16 +1,13 @@
 package com.liu.gittest;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
