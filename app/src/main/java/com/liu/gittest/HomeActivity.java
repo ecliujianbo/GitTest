7f088b061635f464145d7f46820347e48256cf2c
liujianbo <ecliujianbo@126.com>
2016-01-09 12:54:00 +0800

    edit in home

---
+++ app/src/main/java/com/liu/gittest/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.liu.gittest;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by jianbo on 2016/1/9.
+ */
+public class HomeActivity extends FragmentActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
